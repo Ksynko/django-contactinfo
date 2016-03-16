83bfc76458ace21f2a3191ebfb156023c1fa9b1a
Kseniia Pinihina <lutiks2012@gmail.com>
2016-03-16 20:55:01 +0200

    Update models.py

--- contactinfo/models.py
+++ contactinfo/models.py
@@ -1,6 +1,6 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.conf import settings
-from __future__ import unicode_literals
 
 from django_countries.fields import CountryField
 
